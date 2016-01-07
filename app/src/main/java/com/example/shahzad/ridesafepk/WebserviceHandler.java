dfe922cf69ef4df054d55f89c2dd9f246634aa93
Muhammad Shahzad <shahzadthathal@gmail.com>
2016-01-08 03:55:16 +0500

    uploading changes

--- app/src/main/java/com/example/shahzad/ridesafepk/WebserviceHandler.java
+++ app/src/main/java/com/example/shahzad/ridesafepk/WebserviceHandler.java
@@ -18,8 +18,11 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by shahzad on 12/12/2015.
@@ -27,6 +30,10 @@ import java.util.ArrayList;
 public class WebserviceHandler {
 
     String _error;
+
+    private String Content;
+    private String Error = null;
+
 
     public  WebserviceHandler(Context context)
     {
@@ -74,8 +81,8 @@ public class WebserviceHandler {
             String line = null;
             while ((line = reader.readLine()) != null)
             {
-               // sb.append(line + "\n");
-                sb.append(line).append("\n");
+                sb.append(line + "\n");
+               // sb.append(line).append("\n");
             }
             is.close();
             result = sb.toString();
@@ -85,9 +92,6 @@ public class WebserviceHandler {
             _error=e.getMessage();
             Log.e("log_tag", "Error Converting Result"+ e.toString());
         }
-
-
-
 
         result=result.replace("\\u000d\\u000a", "\n");
         result=result.replace("\"", "");
@@ -100,11 +104,82 @@ public class WebserviceHandler {
         //return jArray;
     }
 
+    private String GetJsonFromUrlNew(String urls) {
+
+        // added by Sir Shahid, to avoid some http request stoped by java
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().build();
+        StrictMode.setThreadPolicy(policy);
+
+
+        BufferedReader reader=null;
+        try {
+            URL url = new URL(urls);
+            URLConnection conn = url.openConnection();
+            conn.setDoOutput(true);
+            reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+
+            // Read Server Response
+            while ((line = reader.readLine()) != null) {
+                // Append server response in string
+                sb.append(line);
+            }
+            Content = sb.toString();
+        }
+        catch(Exception ex)
+        {
+            Error = ex.getMessage();
+        }
+        finally
+        {
+            try
+            {
+                reader.close();
+            }
+
+            catch(Exception ex) {}
+        }
+        return  Content;
+    }
+
+
     public RideModel AddRide(Context context,RideModel obj) throws UnsupportedEncodingException
     {
-        String url = context.getResources().getString(R.string.SERVICE_URL)+ "AddRide/" + URLEncoder.encode(String.valueOf(obj.passengerID))  + "/" + URLEncoder.encode(String.valueOf(obj.driverID)) + "/" + URLEncoder.encode(String.valueOf(obj.from_destination)) + "/" + URLEncoder.encode(String.valueOf(obj.to_destination)) +"/" + URLEncoder.encode(String.valueOf(obj.from_lat)) + "/" + URLEncoder.encode(String.valueOf(obj.from_lng)) +"/"+ URLEncoder.encode(String.valueOf(obj.to_lat)) +"/"+ URLEncoder.encode(String.valueOf(obj.to_lng));
-        String jsonResult = GetJsonFromUrl(url);
-        Log.d("Add Job Url",url);
+
+
+        String url = context.getResources().getString(R.string.SERVICE_URL)+ "AddRide/" + URLEncoder.encode(String.valueOf(obj.passengerID))  + "/" + URLEncoder.encode(String.valueOf(obj.driverID)) + "/" + URLEncoder.encode(String.valueOf(obj.from_destination), "UTF-8") + "/" + URLEncoder.encode(String.valueOf(obj.to_destination), "UTF-8") +"/" + URLEncoder.encode(String.valueOf(obj.from_lat)) + "/" + URLEncoder.encode(String.valueOf(obj.from_lng)) +"/"+ URLEncoder.encode(String.valueOf(obj.to_lat)) +"/"+ URLEncoder.encode(String.valueOf(obj.to_lng));
+       // String jsonResult = GetJsonFromUrl(url);
+        Log.d("Add Job Url", url);
+        String jsonResult = GetJsonFromUrlNew(url);
+        Log.d("Add job Response",jsonResult+"");
+        if (jsonResult != null) {
+            JSONObject jsonResponse;
+            try {
+                jsonResponse = new JSONObject(jsonResult);
+                Integer id       = jsonResponse.optInt("id");
+                Integer passengerID       = jsonResponse.optInt("passengerID");
+                Integer driverID       = jsonResponse.optInt("driverID");
+                String from_address     = jsonResponse.optString("from_destination").toString();
+                String to_address = jsonResponse.optString("to_destination").toString();
+                double from_lat = jsonResponse.optDouble("from_lat");
+                double from_lng = jsonResponse.optDouble("from_lng");
+                double to_lat = jsonResponse.optDouble("to_lat");
+                double to_lng = jsonResponse.optDouble("to_lng");
+                Integer status = jsonResponse.optInt("status");
+
+               // String date_created = jsonResponse.optString("date_created");
+                RideModel returnRide = new RideModel(id, passengerID, driverID, from_address, to_address, from_lat, from_lng, to_lat,to_lng,status);
+
+                // RideModel returnRide = new RideModel( ride.getInt("id"), ride.getInt("passengerID"), ride.getInt("driverID"), ride.getString("from_destination"), ride.getString("to_destination"), Double.parseDouble(ride.getString("from_lat")), Double.parseDouble(ride.getString("from_lng")), Double.parseDouble(ride.getString("to_lat")), Double.parseDouble(ride.getString("to_lng")), ride.getInt("status"));
+                return returnRide;
+            }
+            catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        /*
         if(jsonResult!="")
         {
             try {
@@ -115,6 +190,7 @@ public class WebserviceHandler {
                 e.printStackTrace();
             }
         }
+        */
         return null;
     }
 
